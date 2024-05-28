13fdc2f57143f73e224b86d6c45600f6fd0298b0
zhuruishu0848 <115624625+zhuruishu0848@users.noreply.github.com>
2024-05-28 21:46:57 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from layers import *
-
-
-
-
 
 class GNN(nn.Module):
     def __init__(self, args, input_dim, output_dim, hidden_dim, gru_step, dropout_p):
@@ -21,7 +14,7 @@ class GNN(nn.Module):
         self.LSTM = nn.LSTM(
             input_size=self.input_dim,
             hidden_size=self.hidden_dim,#96
-            num_layers=1,#lstm层数
+            num_layers=1,#lstm layer
             batch_first=True,#(batch,seq,dim)
             bidirectional=False
         )
@@ -46,7 +39,6 @@ class GNN(nn.Module):
             args = args,
             input_dim = self.hidden_dim,
             output_dim = self.nhid,
-            # output_dim = self.hidden_dim,
             act = torch.nn.Tanh(),
             dropout_p = self.dropout_p,
             gru_step = self.gru_step
@@ -64,12 +56,8 @@ class GNN(nn.Module):
             self.ReadoutLayer]
 
 
-    def forward(self, feature, support, mask):#train_feature[idx], train_adj[idx], train_mask[idx]、
-        # dv = 'cuda' if feature.is_cuda else 'cpu'
-        # N = support.size()[-1]
-        # support += torch.eye(N, device=dv)
+    def forward(self, feature, support, mask):
         support = torch.mul(support, self.GSLLayer(feature, support))
-        # support = self.GSLLayer(feature, support)
         feature, _ = self.LSTM(feature, None)
         activations = [feature]
         for index, layer in enumerate(self.layers):
